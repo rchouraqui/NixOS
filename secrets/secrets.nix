3f1d609501ed13969798f0fe95f3ae5391bcfaa1
rchouraqui <raphaelchouraqui92@gmail.com>
2026-08-25 13:20:02 +0200

    build(secrets[secrets]): create secrets.nix

---
+++ secrets/secrets.nix
@@ -0,0 +1,14 @@
+let
+  srv-nuc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF8h0U5Dbp8NxS79xv4VSbDt8lS0GbA8R2Uvjy6CL1dk root@srv-nuc";
+  #add srv-fix = 
+  framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGgR92yn3ANd1n1s+jbGRM/+Y1iV7GEYh2fDLLp+8PdL root@nixos-framework";
+
+  systems = [
+    srv-nuc
+    framework
+  ];
+
+in
+{
+  "cloudflare-token.age".publicKeys = systems;
+}
