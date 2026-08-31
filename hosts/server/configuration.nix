a7297c4e28370e1b9dc772a73532f19746dadde0
rchouraqui <raphaelchouraqui92@gmail.com>
2026-08-31 19:07:51 +0200

    build(hosts[server]): remove networks and drawio and include the network configuration in configuration.nix

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -20,17 +20,27 @@
 
   networking = {
     hostName = "server";
+    firewall.enable = true;
     interfaces.enp2s0.ipv4.addresses = [
       {
         address = "10.0.10.4";
         prefixLength = 24;
       }
     ];
+    defaultGateway = {
+      address = "10.0.10.1";
+      interface = "enp2s0";
+    };
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "8.8.8.8"
+      "8.8.4.4"
+    ];
   };
 
   config-hw = {
     nix-settings = true;
-    network = true;
     keyboard = true;
     bluetooth = false;
     printer = false;
